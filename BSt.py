92a820776e99cb535f0b04641d4743de48c43199
smuthusamy <saravanamuthu.muthusamy@slu.edu>
2023-07-13 10:02:25 -0500

    linked list

---
+++ BSt.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self,value):
+        self.right=None
+        self.left=None
+        self.value=value
+class BST():
+    def __init__(self):
+        self.root=None
+    def insertTree(self,node,value):
+        if self.root is None:
+            self.root=Node(value)
+        else:
+            if node.value < value:
+                if node.left is not None:
+                    self.insertTree(node.left,value)
+                else:
+                    node.left=Node(value)
+            elif node.value > value:
+                if node.right is not None:
+                    self.insertTree(node.right,value)
+                else:
+                    node.right = Node(value)
+            else:
+                print('the value is already present in the node')
+    def printInorder(self, node):
+        if(node!=None):
+            self.printInorder(node.left)
+            print(node.value)
+            self.printInorder(node.right)
+            
+if __name__=="__main__":
+    testTree = BST()
+    testTree.insertTree(testTree.root, 200)
+    testTree.insertTree(testTree.root, 300)
+    testTree.insertTree(testTree.root, 100)
+    testTree.insertTree(testTree.root, 30)
+    testTree.printInorder(testTree.root)
+    
---
+++ linkedlist.py
@@ -0,0 +1,72 @@
+class Node:
+    def __init__(self,value):
+        self.data=value
+        self.next=None
+class LinkedList():
+    def __init__(self) -> None:
+        self.head=None
+    def insertValue(self,value):
+        currentNode=Node(value)
+        if self.head is None:
+            self.head=currentNode
+        else:
+            temp=self.head
+            while temp.next!= None:
+                temp=temp.next
+            temp.next=currentNode
+    def deleteValue(self,targetValue):
+        currentNode=self.head
+        prevNode=None
+        if currentNode.data == targetValue:
+            self.head=currentNode.next
+            print("first node is deleted")
+            return 0
+        else:
+            while currentNode is not None:
+                if currentNode.data==targetValue:
+                    if currentNode.next is None:
+                        prevNode.next=None
+                        print('last node is deleted')
+                        return 
+                    else:
+                        print('middle node is deleted')
+                        prevNode.next=currentNode.next
+                        return 0
+
+                else:
+                    prevNode=currentNode
+                    currentNode=currentNode.next
+    def getIndex(self,targetValue):
+        currentNode =self.head
+        count=0
+        while currentNode is not None:
+            if currentNode.data == targetValue:
+                print("the index of the target value is"+" "+str((count+1)))
+                return 
+            else:
+                currentNode = currentNode.next
+                count+=1
+    def reverseList(self):
+        prevNode=None
+        currentNode=self.head
+        while currentNode is not None:
+            nextNode=currentNode.next
+            currentNode.next =prevNode
+            prevNode=currentNode
+            currentNode=nextNode
+        self.head=prevNode
+    def printList(self):
+        currentNode=self.head
+        while currentNode is not None:
+            print(currentNode.data)
+            currentNode=currentNode.next
+if __name__=="__main__":
+    obj1=LinkedList()
+    
+    obj1.insertValue(5)
+    obj1.insertValue(6)
+    obj1.insertValue(7)
+    obj1.insertValue(9)
+    obj1.reverseList()
+    obj1.getIndex(7)
+    obj1.printList()
