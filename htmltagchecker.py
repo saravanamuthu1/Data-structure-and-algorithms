315a01134963d89b777d9326ad763af05df009b4
smuthusamy <saravanamuthu.muthusamy@slu.edu>
2023-07-19 18:25:57 -0500

    html tag checker

---
+++ htmltagchecker.py
@@ -0,0 +1,21 @@
+from stack import Arraystack
+raw ="<html></html><P>"
+def parser(raw_text):
+    stack_object = Arraystack()
+    start_index = raw_text.find('<')
+    while start_index != -1:
+        end_index = raw_text.find('>',start_index+1)
+        if end_index == -1:
+            return False
+        tag = raw_text[start_index+1:end_index]
+        if not tag.startswith('/'):
+            stack_object.push(tag)
+        else:
+            if stack_object.is_empty():
+                return False
+            if tag[1:] != stack_object.pop():
+                return False
+        start_index = raw_text.find("<",end_index+1)
+    return stack_object.is_empty()
+if __name__ == "__main__":
+    print(parser(raw))
